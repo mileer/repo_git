3b8d014fb019dcacf8d2ba037f2e59337234a876
mileer <mileer@mail.ru>
2013-11-13 17:03:09 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RepoGit::Application.config.secret_key_base = 'd25df29474ef914c7a6550d8cdd82edbfbdc4bc1e2228b8a27abad6e8b6d7c312ae5eeedd9ceeb64881f4e738abc74a7cd42fdb2810288a1b4edd90921a02930'
